d509fe70a90b82bebf6c07e15df929c8d945b442
Jonasson Oscar <oscjjonasson@gmail.com>
2022-04-24 13:28:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Reps</title>
+</head>
+<body>
+    
+    <input/>
+    <input/>
+    <input/>
+    <input/>
+    
+</body>
+</html>
