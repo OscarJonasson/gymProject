6812e3dbee9800204b3fb0c45d8f2a09d5b008c4
Jonasson Oscar <oscjjonasson@gmail.com>
2022-04-27 19:34:55 +0300

    fixed redirecting for sending additions, index works with tablename

---
+++ create.php
@@ -0,0 +1,165 @@
+<?php 
+include 'db.php';
+
+function test_inputs($data){
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if(isset($_POST['table'])){
+    $nameOfTable = test_inputs($_POST['nameOfTable']);
+
+    $query = "CREATE TABLE `$nameOfTable` (
+        `id` int NOT NULL AUTO_INCREMENT,
+        `exercise` varchar(100) NOT NULL,
+        `sets` int NOT NULL,
+        `repetitions` int NOT NULL,
+        `weight` int NOT NULL,
+        `musclegroup` varchar(50) NOT NULL,
+        `rir` int NOT NULL,
+        PRIMARY KEY (`id`)
+       ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+       
+       $result = mysqli_query($connection, $query);
+}
+
+
+if(isset($_POST['submit'])){
+    $tablename = $_POST['tablename'];
+    $exercise = test_inputs($_POST['exercise']);
+    $sets = $_POST['sets'];
+    $reps = $_POST['repetitions'];
+    $weight = $_POST['weight'];
+    $musclegroup = $_POST['musclegroup'];
+    $rir = $_POST['rir'];
+
+
+$query = "INSERT into `$tablename` values(NULL,'$exercise', $sets,$reps,$weight,'$musclegroup', $rir)";
+$result = mysqli_query($connection, $query);
+// $query = "INSERT INTO todos(task,date) VALUES('$task','$date')"; 
+// $query = "INSERT into $tablename values(null, '$exercise', $sets, $reps, $weight, '$musclegroup', $rir)";
+
+// $result = mysqli_query($connection, $query);
+
+    
+
+    if(!$result){
+        die("Could not post. Sorry.");
+     }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Reps</title>
+</head>
+<body>
+<header>
+        <nav>
+            <a href="./index.php">Programs</a>
+            <a href="./create.php">Create</a>
+        </nav>
+    </header>
+    <section>
+    <h2>Create your program</h2>
+    <div class="create__container">
+        <h3>Program name</h3>
+
+        <form action="create.php?tablename=" method="POST">
+            <div class="separator">
+            <label for="nameOfTable">Program name</label>
+            <input type="text" name="nameOfTable" id="nameOfTable">
+            </div>
+            <button name="table">Submit</button>
+        </form>
+
+        <form action="create.php?tablename=<?=$_GET['tablename']?>" method="POST">
+           <input type="text" name="tablename" value=<?=$_GET['tablename']?>>
+            <div class="separator">
+                <label for="exercise">Exercise</label>
+                <input type="text" name="exercise" id="exercise" required>
+            </div>
+            <div class="separator">
+                <label for="sets">Sets</label>
+                <input type="number" name="sets" id="sets" required>
+            </div>
+            <div class="separator">
+                <label for="reps">Reps</label>
+                <input type="number" name="repetitions" id="reps" required>
+            </div>
+            <div class="separator">
+                <label for="weight">Weight</label>
+                <input type="number" name="weight" id="weight" required>
+            </div>
+            <div class="separator">
+
+                <label for="musclegroup">Muscle group</label>
+                <select type="text" name="musclegroup" id="musclegroup" required>
+                    <option value="upper body">Upper Body</option>
+                    <option value="lower body">Lower Body</option>
+                </select>
+            </div>
+            <div class="separator">
+                <label for="rir">RIR</label>
+                <input type="number" name="rir" id="rir">
+            </div>
+            <button name="submit">Submit</button>
+        </form>
+
+
+        <form action="create.php" action="GET">
+            <select name="tablename">
+           <?php $query = "SHOW TABLES";
+                $result = mysqli_query($connection, $query);
+                if(!$result){
+                    die('No exercises to get');
+                  };
+                  while($row = mysqli_fetch_assoc($result)){
+                    $tablesin = $row['Tables_in_gym'];
+                    ?>
+                  <option value=<?= $tablesin ?>><?=$tablesin?></option>
+                  <?php
+                } ?>
+            </select>
+            <button>Submit</button>
+        </form>
+
+        <section class="listCurrent">
+        <?php 
+        $query = "SELECT * from {$_GET['tablename']}";
+        $result = mysqli_query($connection, $query);
+        if(!$result){
+            die('No exercises to get');
+          };
+          while($row = mysqli_fetch_assoc($result)){
+              $id = $row['id'];
+              $exercise = $row['exercise'];
+              $sets = $row['sets'];
+              $repetitions = $row['repetitions'];
+              $weight = $row['weight'];
+              $musclegroup = $row['musclegroup'];
+              $rir = $row['rir'];  
+              ?> 
+
+              <p>
+                <?= $id, $exercise, $sets ."x". $repetitions, $weight ." kg". $musclegroup, $rir?> </p>
+              <?php
+          };
+        ?>
+        </section>
+
+    </div>
+
+    </section>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'db.php';
 
-$daySelect = $_GET['daySelect'] ?  $_GET['daySelect'] : "workouts"; 
+// $daySelect = $_GET['daySelect'] ?  $_GET['daySelect'] : "workouts"; 
 
 // function plusOne(){
 //     include 'db.php';
@@ -16,11 +16,37 @@ $daySelect = $_GET['daySelect'] ?  $_GET['daySelect'] : "workouts";
 //     }
 // }
 
+
+
 if (isset($_POST['addOne'])){
     $id= $_POST['id'];
     $weight = $_POST['weight'];
 
-    $query = "UPDATE {$_GET['daySelect']} SET weight=$weight+1 where id = $id";
+    $query = "UPDATE {$_GET['tablename']} SET weight=$weight+1 where id = $id";
+    
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+      die("Update query failed" . mysqli_error($connection));
+    }
+};
+
+if (isset($_POST['addFive'])){
+    $id= $_POST['id'];
+    $weight = $_POST['weight'];
+
+    $query = "UPDATE {$_GET['tablename']} SET weight=$weight+5 where id = $id";
+    
+    $result = mysqli_query($connection, $query);
+    if (!$result) {
+      die("Update query failed" . mysqli_error($connection));
+    }
+};
+
+if (isset($_POST['addTen'])){
+    $id= $_POST['id'];
+    $weight = $_POST['weight'];
+
+    $query = "UPDATE {$_GET['tablename']} SET weight=$weight+10 where id = $id";
     
     $result = mysqli_query($connection, $query);
     if (!$result) {
@@ -42,20 +68,29 @@ if (isset($_POST['addOne'])){
     <header>
         <nav>
             Programs
-            Add
+            <a href="./create.php">Create</a>
         </nav>
     </header>
     <section>
-        <form action="index.php" method="GET">
-            <select name="daySelect" required>
-                <option invalid  hidden>Select day</option>
-                <option value="workouts">Day 1</option>
-                <option value="workouts2">Day 2</option>
+    <form action="index.php" action="GET">
+            <select name="tablename">
+           <?php $query = "SHOW TABLES";
+                $result = mysqli_query($connection, $query);
+                if(!$result){
+                    die('No exercises to get');
+                  };
+                  while($row = mysqli_fetch_assoc($result)){
+                    $tablesin = $row['Tables_in_gym'];
+                    ?>
+                  <option value=<?= $tablesin ?>><?=$tablesin?></option>
+                  <?php
+                } ?>
             </select>
-            <button>Change</button>
+            <button>Submit</button>
         </form>
+        
         <?php 
-        $query = "SELECT * from {$_GET['daySelect']}";
+        $query = "SELECT * from {$_GET['tablename']}";
         $result = mysqli_query($connection, $query);
         if(!$result){
             die('No exercises to get');
@@ -71,7 +106,7 @@ if (isset($_POST['addOne'])){
               
               
               ?> 
-              <form action="index.php?daySelect=<?=$daySelect?>"  method="POST">
+              <form action="index.php?tablename=<?=$_GET['tablename']?>"  method="POST">
               <p><select name="id" class="hideMe">
                   <option value="<?=$id?>"><?=$id?></option>
               </select> <input class="hideMe" name="weight" value="<?= $weight?>"/><p name="weight"> <?=$weight?> </p> <?= $id, $exercise, $sets ."x". $repetitions, $weight ." kg". $musclegroup, $rir?> </p>
