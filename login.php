6722c2fef6490c37c54bbab5c32987b594b7f67a
Jonasson Oscar <oscjjonasson@gmail.com>
2022-05-01 19:15:17 +0300

    login page styling and inputs

--- login.php
+++ login.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
     <title>Reps</title>
 </head>
 <body>
-    
+
+<main class="container">
+    <div class="entries">
+        <h1>LOGIN</h1>
+        <form>
+            <div class="separator">
+                <label for="username">Username</label>
+                <input type="text" id="username">
+            </div>
+            <div class="separator">
+                <label for="username">Password</label>
+                <input type="text" id="password">
+            </div>
+            <button class="alignme" name="login">Submit</button>
+        </form>
+    </div>
+</main>
+    <?php include './footer.php' ?>
 </body>
 </html>
